466ee86552dfbe64f0f33a854658db3b0f0e34d1
k1au3 <lindquistconcern@gmail.com>
2023-05-29 14:35:11 +0300

    css

---
+++ Bakery/src/js/app.js
@@ -0,0 +1,8 @@
+let infoBtn = document.querySelector('#info');
+let glassBox = document.querySelector('#glass');
+
+function pop(){
+    glassBox.style.display(visible);
+}
+
+infoBtn.onClick(pop());
